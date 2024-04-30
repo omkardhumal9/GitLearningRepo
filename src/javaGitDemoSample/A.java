673834bb00cceff24f6ce1f4bce750134c5e03b2
dhuma <dhuma@192.168.0.104>
2024-04-30 13:43:12 +0530

    A class change

--- src/javaGitDemoSample/A.java
+++ src/javaGitDemoSample/A.java
@@ -5,6 +5,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("Git sample");
 		System.out.println("Second Changes");
+		System.out.println("Third change");
 	}
 
 }
